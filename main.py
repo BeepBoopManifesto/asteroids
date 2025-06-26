1edf3761f29d37c18a43a70dc9f7d64592c4f943
BeepBoopManifesto <kevrut9@gmail.com>
2025-06-26 15:56:15 -0400

    added game loop that draws a black screen, added .gitignore, ignoring venv/ and __pycache__/

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-import pygame, constants 
-
+import pygame
+from constants import *
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, color=0)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
