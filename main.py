b71cfaf19989d8552c0fd70bdcf0ee880d4d2033
BeepBoopManifesto <kevrut9@gmail.com>
2025-06-27 16:40:19 -0400

    added PLAYER_TURN_SPEED to constants, added rotate and update method to Player class, call update in main game loop to rotate player

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +15,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
