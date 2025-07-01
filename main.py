612d2d46cefd452203ce804b2c7bd9189c455116
BeepBoopManifesto <kevrut9@gmail.com>
2025-07-01 16:34:57 -0400

    created asteroid.py and Asteroid class, created asteroidfield.py and Asteroidfield class, added asteroid_field to initialization in main

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updateable.update(dt)
+
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
+
+        #framerate capped at 60
         dt = clock.tick(60) / 1000
 
 
